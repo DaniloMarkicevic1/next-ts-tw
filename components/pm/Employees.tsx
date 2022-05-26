56cb85bbfd42e6849338099941ea141433dc0e24
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2022-05-26 15:45:00 +0200

    PM employee visuals

--- components/pm/Employees.tsx
+++ components/pm/Employees.tsx
@@ -36,13 +36,59 @@ const Employees: React.FC<EmployeesRes> = ({ employeesRes }) => {
             ) : (
                 <ul>
                     {employeesRes.employees.map((employee: Employee) => (
-                        <Card key={employee.id} employeeId={employee.id}>
-                            <p>
-                                Name: {employee.firstName} {employee.lastName}
-                            </p>
-                            <p>E-mail: {employee.email}</p>
-                            <p>City: {employee.city?.name}</p>
-                        </Card>
+                        <>
+                            {console.log(employee)}
+                            <Card key={employee.id} employeeId={employee.id}>
+                                <p className="grid justify-center text-center">
+                                    <span className="text-gray-800">Name:</span>
+                                    {employee.firstName} {employee.lastName}
+                                </p>
+                                <div className="bg-gray-900 w-0.5 h-10"></div>
+                                <p className="grid justify-center text-center">
+                                    <span className="text-gray-800">
+                                        E-mail:
+                                    </span>
+                                    {employee.email}
+                                </p>
+                                <div className="bg-gray-900 w-0.5 h-10"></div>
+                                <p className="grid justify-center text-center">
+                                    <span className="text-gray-800">City:</span>
+                                    {employee.city?.name}
+                                </p>
+                                <div className="bg-gray-900 w-0.5 h-10"></div>
+                                <p className="grid justify-center text-center">
+                                    <span className="text-gray-800">
+                                        Country:
+                                    </span>
+                                    {employee.city?.country.name}
+                                </p>
+                                <div className="bg-gray-900 w-0.5 h-10"></div>
+                                <p className="grid justify-center text-center">
+                                    <span className="text-gray-800">
+                                        Project:
+                                    </span>
+                                    {employee.project?.name}
+                                </p>
+                                <div className="bg-gray-900 w-0.5 h-10"></div>
+                                <p className="grid justify-center text-center">
+                                    <span className="text-gray-800">
+                                        Project Manager:
+                                    </span>
+                                    {
+                                        employee.project?.projectManager
+                                            ?.firstName
+                                    }
+                                    {employee.project?.projectManager?.lastName}
+                                </p>
+                                <div className="bg-gray-900 w-0.5 h-10"></div>
+                                <p className="capitalize grid justify-center text-center">
+                                    <span className="text-gray-800">
+                                        Seniority:
+                                    </span>
+                                    {employee.seniority}
+                                </p>
+                            </Card>
+                        </>
                     ))}
                 </ul>
             )}
