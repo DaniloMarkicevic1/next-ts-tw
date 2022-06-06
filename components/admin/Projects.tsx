60ef4606c2f75d96888e5255fbe83529589c20b7
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2022-06-06 15:05:57 +0200

    fix projects style

--- components/admin/Projects.tsx
+++ components/admin/Projects.tsx
@@ -42,10 +42,37 @@ const Projects = () => {
                 projectsArray.map(({ projectManager, name, id, employees }) => (
                     <section
                         key={id}
-                        className="flex justify-between outline rounded-lg text-gray-300 bg-green-800 items-center p-2 mb-2 hover:outline-gray-800 hover:outline-2 hover:bg-opacity-75"
+                        className="
+                        flex
+                        basis-full
+                        pr-3
+                        items-center
+                        bg-green-800
+                        text-gray-300
+                        mb-2
+                        rounded-md 
+                        z-10
+                        relative    
+                        transition-all
+                        hover:cursor-pointer
+                        after:origin-cursor
+                        after:absolute
+                        after:rounded-full
+                        after:top-0
+                        after:left-0
+                        after:w-full
+                        after:h-full
+                        after:-z-10
+                        after:scale-y-0
+                        after:scale-x-0
+                        after:hover:rounded-md
+                        after:hover:scale-x-100
+                        after:hover:scale-y-100
+                        after:hover:bg-green-600
+                        after:duration-300"
                     >
                         <Link href={`projects/edit/${id}`} passHref>
-                            <a className="w-full h-full">
+                            <a className="w-full h-full pl-2 py-2">
                                 <p>Project Name: {name}</p>
                                 <p>
                                     Project Manager: {projectManager?.firstName}{' '}
